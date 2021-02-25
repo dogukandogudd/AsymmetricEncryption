8aa39a7a81142cced203b7073ef8e37c3624c77b
Doğukan <dogukandogu8@gmail.com>
2021-02-26 01:30:05 +0300

    Add project files.

---
+++ DiffieHellman/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DiffieHellman
+{
+    class Person1
+    {
+        public static byte[] Person1PublicKey;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("-WELCOME TO DIFFIE HELLMAN ENCRYPTION METHOD-");
+            Console.WriteLine("----------------------------------");
+
+            Helman();
+        }
+
+        private static void Helman()
+        {
+            Console.Write("Plase Enter Message to Encrypt : ");
+            string text = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+
+            using (ECDiffieHellmanCng ecd = new ECDiffieHellmanCng())
+            {
+                ecd.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                ecd.HashAlgorithm = CngAlgorithm.Sha256;
+                Person1PublicKey = ecd.PublicKey.ToByteArray();
+
+                Person2 person2 = new Person2();
+
+                CngKey k = CngKey.Import(person2.Person2PublicKey, CngKeyBlobFormat.EccPublicBlob);
+                byte[] senderKey = ecd.DeriveKeyMaterial(CngKey.Import(person2.Person2PublicKey, CngKeyBlobFormat.EccPublicBlob));
+                Send(senderKey, text, out byte[] encryptedMessage, out byte[] IV);
+                person2.Receive(encryptedMessage, IV);
+
+            }
+        }
+
+        public static void Send(byte[] key, string secretMessage, out byte[] encryptedMessage, out byte[] IV)
+        {
+            Console.WriteLine("Message Sending ... ");
+            Console.WriteLine("----------------------------------");
+
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                IV = aes.IV;
+
+                // Encrypt the message
+                using (MemoryStream ms = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    byte[] plainTextMessage = Encoding.UTF8.GetBytes(secretMessage);
+                    cs.Write(plainTextMessage, 0, plainTextMessage.Length);
+                    cs.Close();
+                    encryptedMessage = ms.ToArray();
+
+                }
+            }
+        }
+    }
+
+
+    public class Person2
+    {
+        public byte[] Person2PublicKey;
+        private byte[] Key;
+
+        public Person2()
+        {
+            using (ECDiffieHellmanCng ecd = new ECDiffieHellmanCng())
+            {
+                ecd.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+                ecd.HashAlgorithm = CngAlgorithm.Sha256;
+                Person2PublicKey = ecd.PublicKey.ToByteArray();
+                Key = ecd.DeriveKeyMaterial(CngKey.Import(Person1.Person1PublicKey, CngKeyBlobFormat.EccPublicBlob));
+
+            }
+
+            Console.Write("Encrypted Version : ");
+
+            foreach (byte b in Key)
+            {
+                Console.Write($"{b}, ");
+
+            }
+            Console.WriteLine("\n----------------------------------");
+
+        }
+
+        public void Receive(byte[] encryptedMessage, byte[] IV)
+        {
+            Console.WriteLine("Converting ... ");
+            Console.WriteLine("----------------------------------");
+
+            using (Aes aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = Key;
+                aes.IV = IV;
+
+                // Decrypt and show the message
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(encryptedMessage, 0, encryptedMessage.Length);
+                        cs.Close();
+
+                        string message = Encoding.UTF8.GetString(ms.ToArray());
+                        Console.Write("Resolved : ");
+                        Console.Write(message);
+
+                    }
+                }
+                Console.ReadKey();
+
+
+
+            }
+        }
+    }
+}
---
+++ RivestShamirAdleman/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RivestShamirAdleman
+{
+    class Program
+    {
+        private static RSAParameters publicKey;
+        private static RSAParameters privateKey;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("-WELCOME TO RSA ENCRYPTION METHOD-");
+            Console.WriteLine("----------------------------------");
+            Console.Write("Plase Enter Message to Encrypt : ");
+            string text = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("----------------------------------");
+            RSA(text);
+
+ 
+        }
+
+        private static void RSA(string msg)
+        {
+            createKey();
+            byte[] encrypt = Encrypt(Encoding.UTF8.GetBytes(msg));
+            byte[] decrypt = Decrypt(encrypt);
+
+            Console.WriteLine("Your Message :" + msg);
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Encrypted Version :" + BitConverter.ToString(encrypt).Replace("-", ""));
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Resolved :" + Encoding.UTF8.GetString(decrypt));
+            Console.WriteLine("----------------------------------");
+            Console.ReadKey();
+        }
+
+        static byte[] Decrypt(byte[] input)
+        {
+            byte[] resolve;
+            using (var rsa = new RSACryptoServiceProvider(512))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(privateKey);
+                resolve = rsa.Decrypt(input, true);
+            }
+            return resolve;
+        }
+
+        static byte[] Encrypt(byte[] input)
+        {
+            byte[] sifrele;
+            using (var rsa = new RSACryptoServiceProvider(512))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(publicKey);
+                sifrele = rsa.Encrypt(input, true);
+            }
+            return sifrele;
+        }
+
+        static void createKey()
+        {
+            using (var rsa = new RSACryptoServiceProvider(512))
+            {
+                rsa.PersistKeyInCsp = false;
+                publicKey = rsa.ExportParameters(false);
+                privateKey = rsa.ExportParameters(true);
+            }
+        }
+    }
+}
